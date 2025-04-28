4ea85179521726be19fe69faae9af8bcc6f42cad
George Dewar <george@dewar.co.nz>
2025-04-28 21:48:43 +1200

    Automatically deal with local URL

--- scripts/run-local.sh
+++ scripts/run-local.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
+cp ./LocalSettings.php ./LocalSettings.php.local
+echo '$wgServer = "http://localhost:8080";' >> ./LocalSettings.php.local
+
 docker run -it \
     -p 8080:80 \
     --add-host host.docker.internal:host-gateway \
-    -v ./LocalSettings.php:/var/www/html/LocalSettings.php \
+    -v ./LocalSettings.php.local:/var/www/html/LocalSettings.php \
     ghcr.io/georgedewar/carhackingwiki
